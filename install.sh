24c149bbddbd4f44efd93342ca5d1382930734b6
timvisee <timvisee@gmail.com>
2017-02-07 16:58:57 +0100

    Improve bash installation script to match installation name

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Header
-echo "dotfiles installation"
+echo "Dotfiles installer"
 echo
 
-# Set up the given file
-# $1: Source file to set up.
+# Install the given file
+# $1: Source file to install.
 # $2: Destination file relative to home directory.
-setup_home_file() {
+install_home_file() {
     # Copy
-    echo "Setting up $1";
-    /bin/cp -T $1 ~/$2
+    echo "Installing $2";
+    sudo cp -T $1 ~/$2
 
     # Return
     return 0
 }
 
 # Setup dotfiles
-echo "Setting up all dotfiles..."
-setup_home_file ./git/.gitconfig.local .gitconfig.local
-setup_home_file ./vim/.vimrc .vimrc
-setup_home_file ./vim/.vimrc .ideavimrc
+echo "Installing all dotfiles..."
+install_home_file ./git/.gitconfig.local .gitconfig.local
+install_home_file ./vim/.vimrc .vimrc
+install_home_file ./vim/.vimrc .ideavimrc
 
 # Done
 echo
-echo "Done"
+echo "Done."
 exit
