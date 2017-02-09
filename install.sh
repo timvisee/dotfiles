fbe3cc463ea9021bbadfb7b88e6fdc97fb17099e
timvisee <timvisee@gmail.com>
2017-02-09 16:31:24 +0100

    Improve and document logging functions

--- install.sh
+++ install.sh
@@ -9,38 +9,49 @@ echo "Dotfiles installer"
 echo "by Tim Visee, timvisee.com"
 echo
 
-print_success() {
+# Log a success message.
+# $1: Message.
+log_success() {
   # Print a message in green with a checkmark
   printf "\e[0;32m[✔] $1\e[0m\n"
 }
 
-print_error() {
+# Log an error message.
+# $1: Message.
+log_error() {
   # Print a message in red with a cross
   printf "\e[0;31m[✖] $1 $2\e[0m\n"
 }
 
-print_question() {
+# Log a question message.
+# $1: Message.
+log_question() {
   # Print a message in orange with a question mark
   printf "\e[0;33m[?] $1\e[0m"
 }
 
-print_info() {
+# Log an info message.
+# $1: Message
+log_info() {
   # Print an indented message in purple
   printf "\n\e[0;35m    $1\e[0m\n\n"
 }
 
-print_result() {
+# Log a result message.
+# $1: Exit code.
+# $2: Message.
+# $3: "true" to exit on error.
+log_result() {
   [ $1 -eq 0 ] \
     && print_success "$2" \
     || print_error "$2"
 
-  [ "$3" == "true" ] && [ $1 -ne 0 ] \
-    && exit
+  [ "$3" == "true" ] && [ $1 -ne 0 ] && exit
 }
 
-# Symlink a dotfile to a file in the dotfiles repository
-# $1: File in repository
-# $2: Target dotfile
+# Symlink a dotfile to a file in the dotfiles repository.
+# $1: File in repository.
+# $2: Target dotfile.
 symlink_file() {
 	if [[ -e "$2" ]]; then
 		echo "Backing up $2 to $2.old..."
@@ -53,8 +64,8 @@ symlink_file() {
 	return 0
 }
 
-# Unlink a previously symlinked dotfile
-# $1: File to unlink
+# Unlink a previously symlinked dotfile.
+# $1: File to unlink.
 unlink_file() {
 	echo "Unlinking '$1'..."
 	rm $1
@@ -67,7 +78,7 @@ unlink_file() {
 	return 0
 }
 
-# Install the given file
+# Install the given file.
 # $1: Source file to install.
 # $2: Destination file relative to home directory.
 install_home_file() {
