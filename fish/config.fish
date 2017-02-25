3e7d8b2ddf8121ce0040820bca6251dba726eb9d
timvisee <timvisee@gmail.com>
2017-02-25 20:03:43 +0100

    Add rust cargo to path in fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,8 @@ eval (thefuck --alias | tr '\n' ';')
 
 # Source the prompt
 source ~/.config/fish/prompt.fish
+
+# Add rust binaries to the path
+if test -d "$HOME/.cargo/bin"
+    set PATH $HOME/.cargo/bin $PATH
+end
