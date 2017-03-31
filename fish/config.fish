f7732be9042607db56b258cd04f0516e4891a7f4
timvisee <timvisee@gmail.com>
2017-03-31 19:03:48 +0200

    Add NVM commands to fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,9 @@ eval (thefuck --alias | tr '\n' ';')
 # Source the prompt
 source ~/.config/fish/prompt.fish
 
+# Source the NVM configuration
+source ~/.config/fish/nvm.fish
+
 # Add rust binaries to the path
 if test -d "$HOME/.cargo/bin"
     set PATH $HOME/.cargo/bin $PATH
