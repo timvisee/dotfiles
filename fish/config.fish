9928b6082e4c1c37d05de5ab9113036850be9c2f
timvisee <timvisee@gmail.com>
2017-05-09 16:39:19 +0200

    Configure Go environment in Bash and Fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,10 @@ source ~/.config/fish/transfer.fish
 if test -d "$HOME/.cargo/bin"
     set PATH $HOME/.cargo/bin $PATH
 end
+
+# Configure Go's environment
+if test -d "/usr/share/go"
+    set -x GOROOT /usr/share/go
+    set -x GOPATH $HOME/.go
+    set -x PATH $PATH $GOROOT/bin $GOPATH/bin
+end
