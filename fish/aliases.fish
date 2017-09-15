f71e4d01630ced0223da930a4861a324acfcc943
timvisee <timvisee@gmail.com>
2017-09-15 11:09:27 +0200

    Create aliases to show/hide files in macOS Finder

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -10,3 +10,10 @@ alias tmux='tmux -2'
 if test -e "/usr/local/bin/composer.phar"
     alias composer="php /usr/local/bin/composer.phar"
 end
+
+# Aliases to hide and show hidden files on macOS
+switch (uname)
+    case Darwin
+        alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+        alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+end
