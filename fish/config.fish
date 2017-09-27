b2b0781d4cf2c4ceb734e5ee256f09c77b73d5a8
timvisee <timvisee@gmail.com>
2017-09-27 09:47:55 +0200

    Fix Docker clean functions

--- fish/config.fish
+++ fish/config.fish
@@ -29,9 +29,9 @@ end
 # Remove all Docker containers
 function docker_remove_containers
     set DOCKER_CONTAINERS (docker ps -aq --no-trunc)
-    if [ $DOCKER_CONTAINERS ]
+    if test -n "$DOCKER_CONTAINERS"
         echo "Removing all Docker containers..."
-        docker rm $DOCKER_CONTAINERS
+        docker rm "$DOCKER_CONTAINERS"
     else
         echo "No Docker containers to remove"
     end
@@ -42,17 +42,17 @@ end
 # Clean Docker, by removing things like dangling images.
 function docker_clean
     set DANGLING_IMAGES (docker images -f "dangling=true" -q --no-trunc)
-    if [ $DANGLING_IMAGES ]
+    if test -n "$DANGLING_IMAGES"
         echo "Cleaning up dangling Docker images..."
-        docker rmi $DANGLING_IMAGES
+        docker rmi "$DANGLING_IMAGES"
     else
         echo "No dangling Docker images to remove"
     end
 
     set DANGLING_VOLUMES (docker volume ls -f "dangling=true" -q)
-    if [ $DANGLING_VOLUMES ]
+    if test -n "$DANGLING_VOLUMES"
         echo "Pruning all unused Docker volumes..."
-        docker volume prune -f $DANGLING_VOLUMES
+        docker volume prune -f
     else
         echo "No dangling Docker volumes to prune"
     end
