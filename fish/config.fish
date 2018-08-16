6b46e3f8ffb37d30c3ca0a8a0d77b70d0da911f9
timvisee <timvisee@gmail.com>
2018-08-16 21:12:16 +0200

    Show a much nicer Vi mode indicator in the fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -153,6 +153,29 @@ if test -n (which keychain)
     and source $SSH_AGENT_ENV
 end
 
+# Nicely prompt the current Vim mode
+function fish_mode_prompt --description 'Displays the current mode'
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --bold red
+                echo 🅽
+            case insert
+                set_color --bold green
+                echo 🅸
+            case replace-one
+                set_color --bold green
+                echo 🆁
+            case visual
+                set_color --bold brmagenta
+                echo 🆅
+        end
+        set_color normal
+        printf "  "
+    end
+end
+
 # Set the preferred editor
 set -x EDITOR vim
 
