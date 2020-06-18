b1c8cc905f084f0e95095011630433a15b669de0
timvisee <tim@visee.me>
2020-06-18 18:15:50 +0200

    Fix last added function, actually kill containers instead of removing

--- fish/config.fish
+++ fish/config.fish
@@ -76,7 +76,7 @@ function docker_kill_all
     set DOCKER_CONTAINERS (docker ps -q --no-trunc)
     if test -n "$DOCKER_CONTAINERS"
         echo "Killing all running Docker containers..."
-        docker rm $DOCKER_CONTAINERS
+        docker kill $DOCKER_CONTAINERS
     else
         echo "No Docker containers to kill"
     end
